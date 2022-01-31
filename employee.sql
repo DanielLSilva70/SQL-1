2b3d9b0df9faf2148064230db67bf8597cb639b6
Daniel Silva <daniel@Daniels-MacBook-Air.local>
2022-01-31 15:46:38 -0600

    employee done

---
+++ employee.sql
@@ -0,0 +1,5 @@
+select first_name, last_name from employee where city ='Calgary';
+ select MAX(birth_date) from employee;
+ select min(birth_date) from employee;
+ select * from employee where reports_to = 2;
+ select * from employee where city = 'Lethbridge'
